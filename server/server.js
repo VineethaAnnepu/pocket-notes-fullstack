4833bb282d41c6ac08a78aab4ee027c386a294a1
VineethaAnnepu <Vineethaannepu@gmail.com>
2025-09-03 14:56:05 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: 'https://pocket-notes-fullstack.onrender.com',
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
